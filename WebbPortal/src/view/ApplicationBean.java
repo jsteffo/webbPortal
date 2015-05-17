687a1f575b4cedcd77a1287587273984dc0ed311
StefanJ <StefanJ@Stefan>
2015-05-18 00:58:30 +0200

    e

---
+++ WebbPortal/src/view/ApplicationBean.java
@@ -0,0 +1,21 @@
+package view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ApplicationBean {
+
+	public void broadBandButton(){
+		
+	}
+	
+	public void fireWallButton(){
+		
+	}
+	
+	public void natButton(){
+		
+	}
+}
